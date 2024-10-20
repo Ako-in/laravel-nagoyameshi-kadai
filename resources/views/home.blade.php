0310cfa719c9b7b3c90208842cc5a1dc47fb5ce3
Ako-in <sinba.805i@gmail.com>
2024-10-20 23:04:48 +0700

    画像ビュー修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -58,7 +58,9 @@
                     <a href="{{ route('restaurants.show', $highly_rated_restaurant) }}" class="link-dark nagoyameshi-card-link">
                         <div class="card h-100">
                             @if ($highly_rated_restaurant->image !== '')
-                                <img src="{{ asset('storage/restaurants/' . $highly_rated_restaurant->image) }}" class="card-img-top nagoyameshi-vertical-card-image">
+                                {{-- <img src="{{ asset('storage/restaurants/' . $highly_rated_restaurant->image) }}" class="card-img-top nagoyameshi-vertical-card-image"> --}}
+                                <img src="{{ Storage::disk('s3')->temporaryUrl('/' . $highly_rated_restaurant->image,now()->addDay()) }}" alt="Restaurant Image" class="w-100">
+                                {{-- <img src="{{ Storage::disk('s3')->temporaryUrl('/' . $highly_rated_restaurant,now()->addDay()) }}" alt="Restaurant Image" class="w-100"> --}}
                             @else
                                 <img src="{{ asset('/images/no_image.jpg') }}" class="card-img-top nagoyameshi-vertical-card-image" alt="画像なし">
                             @endif
@@ -175,7 +177,8 @@
                     <a href="{{ route('restaurants.show', $new_restaurant) }}" class="link-dark nagoyameshi-card-link">
                         <div class="card h-100">
                             @if ($new_restaurant->image !== '')
-                                <img src="{{ asset('storage/restaurants/' . $new_restaurant->image) }}" class="card-img-top nagoyameshi-vertical-card-image">
+                                {{-- <img src="{{ asset('storage/restaurants/' . $new_restaurant->image) }}" class="card-img-top nagoyameshi-vertical-card-image"> --}}
+                                <img src="{{ Storage::disk('s3')->temporaryUrl('/' . $new_restaurant->image,now()->addDay()) }}" alt="Restaurant Image" class="w-100">
                             @else
                                 <img src="{{ asset('/images/no_image.jpg') }}" class="card-img-top nagoyameshi-vertical-card-image" alt="画像なし">
                             @endif
